f0af92b07992147241385ab66f96c5d15e342c60
Christoph Bessei <28066477+christoph-bessei@users.noreply.github.com>
2024-12-29 15:50:11 +0100

    Adaptions for symfony/cli 5

--- src/WebserverInstaller.php
+++ src/WebserverInstaller.php
@@ -6,10 +6,10 @@ class WebserverInstaller
 {
     public static function isInstalled()
     {
-        exec('"${HOME}/.symfony/bin/symfony php" -r "echo PHP_INT_MAX;" 2>&1', $output);
+        exec('"${HOME}/.symfony5/bin/symfony" php -r "echo PHP_INT_MAX;" 2>&1', $output);
         $output = (int)implode('', $output);
 
-        return $output !== PHP_INT_MAX;
+        return $output === PHP_INT_MAX;
     }
 
     public static function install()
